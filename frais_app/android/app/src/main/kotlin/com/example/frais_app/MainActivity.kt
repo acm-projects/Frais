2874685ea61a755ca10422645c221f462ff71c9e
jbrowning96 <jtb190008@utdalls.edu>
2021-03-25 01:35:31 -0500

    justins fridge project files

---
+++ frais_app/android/app/src/main/kotlin/com/example/frais_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frais_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ frais_app/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(fridge());
+
+class fridge extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: AppBar(
+          title: new Row(
+              children: const <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.menu),
+                  color: Color(0xff),
+                ),
+              ]
+          ),
+          backgroundColor: Color(0xfff7edf0),
+          elevation: 0.0,
+        ),
+        body: new Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 400.0,
+                  height: 160.0,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0,3),
+                      )
+                    ],
+                    color: Color(0xff7AC313),
+                  )
+              ),
+            ),
+            Center(
+                child: Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        width: 400.0,
+                        height: 462.0,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0,3),
+                            )
+                          ],
+                          color: Color(0xfff5EFF4),
+                        ),),
+                      GridView.count(
+                          primary: false,
+                          padding: const EdgeInsets.all(20),
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          crossAxisCount: 3,
+                          shrinkWrap: true,
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text("Bananas"),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Oranges'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Apples'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Lemons'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Peaches'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Strawberries'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text("Grapes"),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Cherries'),
+                              color: Color(0xff7AC313),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const Text('Avocado'),
+                              color: Color(0xff7AC313),
+                            ),
+                          ]
+                      ),
+                    ]
+                ))],),
+        backgroundColor: Color(0xfff7edf0),
+      ),
+
+    );
+  }
+}
