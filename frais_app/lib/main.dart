e757451afa5f78c854c6a847fe545ab945edcf53
jbrowning96 <jtb190008@utdalls.edu>
2021-03-28 00:57:31 -0500

    route pages

--- frais_app/lib/main.dart
+++ frais_app/lib/main.dart
@@ -1,123 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import  'home.dart';
 
-void main() => runApp(fridge());
+void main() => runApp(MyApp());
 
-class fridge extends StatelessWidget{
+class MyApp extends StatelessWidget {
+
+  // Root of the app
+
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: AppBar(
-          title: new Row(
-              children: const <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.menu),
-                  color: Color(0xff),
-                ),
-              ]
-          ),
-          backgroundColor: Color(0xfff7edf0),
-          elevation: 0.0,
-        ),
-        body: new Column(
-          children: <Widget>[
-            Center(
-              child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 400.0,
-                  height: 160.0,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0,3),
-                      )
-                    ],
-                    color: Color(0xff7AC313),
-                  )
-              ),
-            ),
-            Center(
-                child: Stack(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10.0),
-                        width: 400.0,
-                        height: 462.0,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0,3),
-                            )
-                          ],
-                          color: Color(0xfff5EFF4),
-                        ),),
-                      GridView.count(
-                          primary: false,
-                          padding: const EdgeInsets.all(20),
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                          crossAxisCount: 3,
-                          shrinkWrap: true,
-                          children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text("Bananas"),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Oranges'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Apples'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Lemons'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Peaches'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Strawberries'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text("Grapes"),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Cherries'),
-                              color: Color(0xff7AC313),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              child: const Text('Avocado'),
-                              color: Color(0xff7AC313),
-                            ),
-                          ]
-                      ),
-                    ]
-                ))],),
-        backgroundColor: Color(0xfff7edf0),
+
+  Widget build (BuildContext context) {
+
+    return MaterialApp(
+
+      title: 'Frais App',
+
+      theme: ThemeData(
+
+        primarySwatch: Colors.green,
+
       ),
 
-    );
+    //home: SplashPage() this is defined in French-Press splash as a loading
+        // and login page.
+
+    routes: <String, WidgetBuilder>{
+
+      '/': (BuildContext context) => HomeScreen(),
+      '/details': (context) => DetailScreen(),
+      //'/camera':
+    },);
   }
 }
